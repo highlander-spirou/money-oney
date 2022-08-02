70ec06c36c2a380d85cfeb5c1a00c05be1c3cd0e
shyn_linux <highlandetspirou2812@gmail.com>
2022-08-02 21:03:59 +0700

    #3 - add test to scrape data's add function

---
+++ testing/test_datalake/test_add_all_to_addition.py
@@ -0,0 +1,20 @@
+import sys
+from time import perf_counter
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+if __name__ == '__main__':
+        sys.path.insert(0, '/home/lelet/selenium_linux/')
+        from datalake.utils import add_all_additional_info
+        from testing.preprocessor_ import preprocessor_test_module
+        from sqlalchemy import create_engine
+        from decouple import config
+
+        start = perf_counter()
+        _, clean_src_data = preprocessor_test_module()
+        engine = create_engine(config('POSTGRES_URI'), echo=True)
+        add_all_additional_info(clean_src_data, engine)
+
+        
+        stop = perf_counter()
+        print(f'module running in {stop-start:.4f} seconds')
---
+++ testing/test_datalake/test_add_all_to_homepage.py
@@ -0,0 +1,20 @@
+import sys
+from time import perf_counter
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent
+if __name__ == '__main__':
+        sys.path.insert(0, '/home/lelet/selenium_linux/')
+        from datalake.utils import add_all_homepage
+        from testing.preprocessor_ import preprocessor_test_module
+        from sqlalchemy import create_engine
+        from decouple import config
+
+        start = perf_counter()
+        clean_table_data, _ = preprocessor_test_module()
+        engine = create_engine(config('POSTGRES_URI'), echo=True)
+        add_all_homepage(clean_table_data, engine)
+
+        
+        stop = perf_counter()
+        print(f'module running in {stop-start:.4f} seconds')
