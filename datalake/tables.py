83e1e0a224d420b728969208cb662bd1277e0ddd
shyn_linux <highlandetspirou2812@gmail.com>
2022-08-02 20:53:40 +0700

    #3 - add AdditionalInfo table

--- datalake/tables.py
+++ datalake/tables.py
@@ -17,6 +17,18 @@ class Table(Base):
     create_at:datetime = Column(DateTime)
 
 
+class AdditionalInfo(Base):
+    __tablename__ = 'infoTable'
+
+    id:int = Column(Integer, primary_key=True)
+    fund:str = Column(String)
+    place_order_time:datetime = Column(DateTime)
+    conversion_fee:float = Column(Float)
+    create_at:datetime = Column(DateTime)
+
+    
+
+
 class HomePageTableFactory(NamedTuple):
     fund:str
     company:str
@@ -25,3 +37,8 @@ class HomePageTableFactory(NamedTuple):
     price_updated_at:datetime
     create_at:datetime = datetime.utcnow() + timedelta(hours=7)
 
+class AdditionalInfoFactory(NamedTuple):
+    fund:str
+    place_order_time:datetime
+    conversion_fee:float
+    create_at:datetime = datetime.utcnow() + timedelta(hours=7)
